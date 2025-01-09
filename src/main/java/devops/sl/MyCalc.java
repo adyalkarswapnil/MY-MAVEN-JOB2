8be2ff3b37f6297f48b18f6e927091ac7c3b80e9
adyalkarswapnil <adyalkarswapnil@gmail.com>
2025-01-10 03:24:26 +0530

    changes

--- src/main/java/devops/sl/MyCalc.java
+++ src/main/java/devops/sl/MyCalc.java
@@ -17,9 +17,9 @@ public class MyCalc {
 	public static void main(String[] args) {
 		
         MyCalc calc = new MyCalc();
-        System.out.println("Sum is : "+calc.sum(20, 10));
-        System.out.println("Diff is : "+calc.diff(20, 10));
-        System.out.println("Mul is : " +calc.mul(20, 10));
+        System.out.println("Sum is : "+calc.sum(30, 10));
+        System.out.println("Diff is : "+calc.diff(30, 10));
+        System.out.println("Mul is : " +calc.mul(30, 10));
 	}
 	
 }

70bd608375dccbc4b3ea85276295d476d9214e60
adyalkarswapnil <adyalkarswapnil@gmail.com>
2025-01-19 17:38:11 +0530

    DemoC class added

--- src/main/java/devops/sl/MyCalc.java
+++ src/main/java/devops/sl/MyCalc.java
@@ -14,12 +14,21 @@ public class MyCalc {
 	{
 	    return a*b;
 	}
+	int div (int a, int b)
+	{
+	    return a/b;
+	}
+	
+	
 	public static void main(String[] args) {
 		
         MyCalc calc = new MyCalc();
+        DemoC ob = new DemoC ();
         System.out.println("Sum is : "+calc.sum(30, 10));
         System.out.println("Diff is : "+calc.diff(30, 10));
         System.out.println("Mul is : " +calc.mul(30, 10));
+        System.out.println("Div is : " +calc.div(30, 10));
+        ob.greet ();
 	}
 	
 }
